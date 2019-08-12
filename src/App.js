90d690d1be7768a71f5672bbed660c833133d73a
Pablograu <paugf89@gmail.com>
2019-08-12 11:05:30 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,34 @@ import CreateTodo from './components/create-todo.component'
 import EditTodo from './components/edit-todo.component'
 import TodosList from './components/todos-list.component'
 
+import logo from './logo.png'
+
+
 function App() {
   return (
     <Router>
       <div className="container">
-        <h2>MERN-STACK Queaseres App</h2>
+        
+        <nav className="nav navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#" target="_blank">
+            <img src={logo} width="30" height="30" alt="cara logo" />
+          </a>
+          <Link to="/" className="navbar-brand">MERN-Stack Queaser App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Queaseres</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Crear Queaseres</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/edit" className="nav-link">Editar Queaseres</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
         <Route path="/" exact component={TodosList} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route path="/create" component={CreateTodo} />
